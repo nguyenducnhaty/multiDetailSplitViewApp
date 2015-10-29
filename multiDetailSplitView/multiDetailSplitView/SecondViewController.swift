21515fd4a040d4b966f2aa058b0b7f854b662940
arled <arledkola@gmail.com>
2015-10-29 18:29:15 +0000

    Swift 2 Multi Detail SplitView Example Initial Commit

---
+++ multiDetailSplitView/multiDetailSplitView/SecondViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SecondViewController.swift
+//  multiDetailSplitView
+//
+//  Created by Arled Kola on 29/10/2015.
+//  Copyright © 2015 arledkola. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
